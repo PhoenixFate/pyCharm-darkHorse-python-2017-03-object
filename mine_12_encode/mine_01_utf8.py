37b830b4ee3061975a64e758e2cbf3cb4bb548fc
phoenix fate <sm516116978@outlook.com>
2020-03-04 15:43:12 +0800

    string and bytes

--- mine_12_encode/mine_01_utf8.py
+++ mine_12_encode/mine_01_utf8.py
@@ -9,3 +9,17 @@ print(hello_str)
 # 在python下面 依然是按照单个字节解析的
 for c in hello_str:
     print(c)
+
+str1 = b'\u4f60\u597d'
+
+str2 = "中文"
+print("%s" % str2.encode())
+
+# string 通过encode方法转为bytes
+# bytes 通过decode转为string
+
+
+str3 = b'\xe4\xb8\xad\xe6\x96\x87'
+print("%s " % str3.decode("utf-8"))
+print("%s" % str1.decode())
+print("%s" % str1.decode('unicode_escape'))
